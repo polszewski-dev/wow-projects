3a07c5e51bfbbd639bd70ca80b8abf8501409c63
Piotr Olszewski <polszewski.dev@gmail.com>
2022-10-15 11:41:09 +0200

    refactor: attribute cache

--- wow-commons/src/main/java/wow/commons/model/attributes/Attributes.java
+++ wow-commons/src/main/java/wow/commons/model/attributes/Attributes.java
@@ -14,14 +14,11 @@ import java.util.stream.Stream;
  * User: POlszewski
  * Date: 2021-03-04
  */
-public class Attributes implements AttributeSource {
+public abstract class Attributes implements AttributeSource {
 	public static final Attributes EMPTY = of(List.of(), Map.of());
 
-	private final List<PrimitiveAttribute> attributeList;
+	protected final List<PrimitiveAttribute> attributeList;
 	private final Map<AttributeId, List<ComplexAttribute>> complexAttributeList;
-
-	private Map<AttributeId, Double> doubleCache;
-	private Map<AttributeId, Percent> percentCache;
 
 	private Attributes(List<PrimitiveAttribute> attributeList, Map<AttributeId, List<ComplexAttribute>> complexAttributeList) {
 		this.attributeList = attributeList;
@@ -29,7 +26,11 @@ public class Attributes implements AttributeSource {
 	}
 
 	public static Attributes of(List<PrimitiveAttribute> attributeList, Map<AttributeId, List<ComplexAttribute>> complexAttributeList) {
-		return new Attributes(attributeList, complexAttributeList);
+		if (attributeList.size() < 10) {
+			return new NonCachedAttributes(attributeList, complexAttributeList);
+		} else {
+			return new CachedAttributes(attributeList, complexAttributeList);
+		}
 	}
 
 	public static Attributes of(List<PrimitiveAttribute> attributeList) {
@@ -46,35 +47,6 @@ public class Attributes implements AttributeSource {
 
 	public static Attributes of(ComplexAttribute complexAttribute) {
 		return of(List.of(), Map.of(complexAttribute.getId(), List.of(complexAttribute)));
-	}
-
-	@Override
-	public double getDouble(AttributeId attributeId) {
-		ensureCache();
-		return doubleCache.getOrDefault(attributeId, 0.0);
-	}
-
-	@Override
-	public Percent getPercent(AttributeId attributeId) {
-		ensureCache();
-		return percentCache.getOrDefault(attributeId, Percent.ZERO);
-	}
-
-	private void ensureCache() {
-		if (doubleCache != null) {
-			return;
-		}
-
-		doubleCache = new EnumMap<>(AttributeId.class);
-		percentCache = new EnumMap<>(AttributeId.class);
-
-		for (Attribute attribute : attributeList) {
-			if (attribute.getId().isDoubleAttribute()) {
-				doubleCache.put(attribute.getId(), attribute.getDouble() + doubleCache.getOrDefault(attribute.getId(), 0.0));
-			} else if (attribute.getId().isPercentAttribute()) {
-				percentCache.put(attribute.getId(), attribute.getPercent().add(percentCache.getOrDefault(attribute.getId(), Percent.ZERO)));
-			}
-		}
 	}
 
 	@Override
@@ -115,5 +87,49 @@ public class Attributes implements AttributeSource {
 		)
 		 .map(Object::toString)
 		 .collect(Collectors.joining(", "));
+	}
+
+	private static class NonCachedAttributes extends Attributes {
+		private NonCachedAttributes(List<PrimitiveAttribute> attributeList, Map<AttributeId, List<ComplexAttribute>> complexAttributeList) {
+			super(attributeList, complexAttributeList);
+		}
+	}
+
+	private static class CachedAttributes extends Attributes {
+		private Map<AttributeId, Double> doubleCache;
+		private Map<AttributeId, Percent> percentCache;
+
+		private CachedAttributes(List<PrimitiveAttribute> attributeList, Map<AttributeId, List<ComplexAttribute>> complexAttributeList) {
+			super(attributeList, complexAttributeList);
+		}
+
+		@Override
+		public double getDouble(AttributeId attributeId) {
+			ensureCache();
+			return doubleCache.getOrDefault(attributeId, 0.0);
+		}
+
+		@Override
+		public Percent getPercent(AttributeId attributeId) {
+			ensureCache();
+			return percentCache.getOrDefault(attributeId, Percent.ZERO);
+		}
+
+		private void ensureCache() {
+			if (doubleCache != null) {
+				return;
+			}
+
+			doubleCache = new EnumMap<>(AttributeId.class);
+			percentCache = new EnumMap<>(AttributeId.class);
+
+			for (Attribute attribute : attributeList) {
+				if (attribute.getId().isDoubleAttribute()) {
+					doubleCache.put(attribute.getId(), attribute.getDouble() + doubleCache.getOrDefault(attribute.getId(), 0.0));
+				} else if (attribute.getId().isPercentAttribute()) {
+					percentCache.put(attribute.getId(), attribute.getPercent().add(percentCache.getOrDefault(attribute.getId(), Percent.ZERO)));
+				}
+			}
+		}
 	}
 }
