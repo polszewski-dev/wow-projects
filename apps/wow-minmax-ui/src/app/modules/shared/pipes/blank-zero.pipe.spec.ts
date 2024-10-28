080af0e008fd59dceb33ba60c8e239d20dfc6fa2
Piotr Olszewski <polszewski.dev@gmail.com>
2024-10-28 14:42:36 +0100

    refactor: deleting spec files

--- apps/wow-minmax-ui/src/app/modules/shared/pipes/blank-zero.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BlankZeroPipe } from './blank-zero.pipe';
-
-describe('BlankZeroPipe', () => {
-	it('create an instance', () => {
-		const pipe = new BlankZeroPipe();
-		expect(pipe).toBeTruthy();
-	});
-});
